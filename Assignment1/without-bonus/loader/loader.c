fa4b72beea97840f011749dbf8895c2b5d021aff
akshaj1103 <singhaksh136@gmail.com>
2025-06-28 20:27:19 +0000

    cleaningup

--- Assignment1/without-bonus/loader/loader.c
+++ Assignment1/without-bonus/loader/loader.c
@@ -126,3 +126,18 @@ int elfChecker(Elf32_Ehdr* header){
  }
  return 1;
 }
+
+int main(int argc, char** argv) 
+{
+  if(argc != 2) {
+    printf("Usage: %s <ELF Executable> \n",argv[0]);
+    exit(1);
+  }
+  // 1. carry out necessary checks on the input ELF file
+  // 2. passing it to the loader for carrying out the loading/execution
+  load_and_run_elf(argv);
+  // 3. invoke the cleanup routine inside the loader
+  loader_cleanup(entry_segment,elfStorage,fileSize,p_filesize_hello);
+  /*cleanup routine is invoked within the load_and_run_elf() function itself*/  
+  return 0;
+}
